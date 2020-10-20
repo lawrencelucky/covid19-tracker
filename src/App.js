cdcde3502d9c600e09f5c07b4b571beb16a62068
lawrencelucky <lawrencelucky1999@gmail.com>
2020-10-20 13:38:50 +0100

    Feat: Adding the layout of the project, pulled the country data from the disease.sh api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from '@material-ui/core';
+
+import InfoBox from './InfoBox';
+import Map from './Map';
+
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+
+  const onCountryChange = async event => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then(response => response.json())
+        .then(data => {
+          const countries = data.map(country => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
     <div className='app'>
-      <h1>The Covid 19 Tracker</h1>
+      <div className='app__left'>
+        <div className='app__header'>
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className='app__dropdown'>
+            <Select
+              variant='outlined'
+              onChange={onCountryChange}
+              value={country}
+            >
+              <MenuItem value='worldwide'>WorldWide</MenuItem>
+              {countries.map(country => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className='app__stats'>
+          <InfoBox title='CoronaVirus Cases' cases={123} total={2000} />
+          <InfoBox title='CoronaVirus Recovered' cases={1234} total={3000} />
+          <InfoBox title='CoronaVirus Death' cases={12345} total={4000} />
+        </div>
+
+        <Map />
+      </div>
+
+      <Card className='app__right'>
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <h3>WorldWide new cases</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
